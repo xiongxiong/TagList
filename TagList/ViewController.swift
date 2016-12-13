be85bfc3a8c513ebed590efd25fbe8fb9ebfdf2a
xiongxiong <xiongxiong0619@gmail.com>
2016-12-13 10:19:01 +0800

    initialize

---
+++ TagList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TagList
+//
+//  Created by 王继荣 on 13/12/2016.
+//  Copyright © 2016 wonderbear. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
